556489811453be42331f2bfcaf502e398eccccd3
Mike Hedman <mikehedman@yahoo.com>
2014-12-26 23:44:15 -0800

    Moving sync code from separate module into this one.

--- ampersand-hoodie-mixin.js
+++ ampersand-hoodie-mixin.js
@@ -1,5 +1,4 @@
-var sync = require('./ampersand-hoodie-sync');
-
+var _ = require('underscore');
 
 module.exports = {
     //implementing classes must include a HOODIE_TYPE session property, with the default argument filled in:
@@ -8,8 +7,64 @@ module.exports = {
         'HOODIE_TYPE': ['string', true]
     },
 
-    //overriding to force our models to use the hoodie version of sync
-    sync: function() {
-        return sync.apply(this, arguments);
+    //This sync function is a modified version of the code in ampersand-sync
+    sync: function (method, model, options) {
+        options = options ? _.clone(options) : {};
+        var jsonPayload;
+        var promise;
+
+        // Ensure that we have a hoodie object type.
+        if (!model.HOODIE_TYPE) {
+            throw new Error('A HOODIE_TYPE property must be specified');
+        }
+
+        // Ensure that we have the appropriate request data.
+        if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
+            jsonPayload = options.attrs || model.toJSON(options) || options.data;
+        }
+
+        switch(method) {
+            case 'read':
+                if (options && options.id) {
+                    promise = window.hoodie.store.find(model.HOODIE_TYPE, options.id);
+                } else {
+                    promise = window.hoodie.store.findAll(model.HOODIE_TYPE);
+                }
+                break;
+            case 'create':
+                if (!jsonPayload) {
+                    throw new Error('Trying to add a model, but no data provided');
+                } else {
+                    promise = window.hoodie.store.add(model.HOODIE_TYPE, jsonPayload, options.silent);
+                }
+                break;
+            case 'update':
+            case 'patch':
+                if (! jsonPayload) {
+                    throw new Error('Trying to update a model, but no data provided');
+                } else if (!model.id) {
+                    throw new Error('Cannot update model - no id provided');
+                } else {
+                    promise = window.hoodie.store.update(model.HOODIE_TYPE, model.id, jsonPayload, options.silent);
+                }
+                break;
+            case 'delete':
+                if (!model.id) {
+                    throw new Error('Cannot delete model - no id provided');
+                } else {
+                    promise = window.hoodie.store.remove(model.HOODIE_TYPE, model.id, options.silent);
+                }
+                break;
+            default:
+                throw new Error('Undefined hoodie method requested: ' + methodMap[method] + ' original: ' + method);
+                break
+        }
+
+        promise.done(function(body) {
+            if (options.success) return options.success(body, 'success');
+        });
+        promise.fail(function(error) {
+            if (options.error) return options.error({}, 'error', error);
+        });
     }
 };
